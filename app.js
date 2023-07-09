0f19264ed6d66339f1d96b6160e04343189ee083
Brahmaraj kashetti <mahaveerkashetti203@gmail.com>
2023-07-09 13:02:31 +0530

    bcrypt hashing

--- app.js
+++ app.js
@@ -3,104 +3,101 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
-const md5 = require('md5');
-//const encrypt = require('mongoose-encryption'); level2
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static('public'))
 
-
 mongoose.connect(
-    "mongodb+srv://mahaveerkashetti203:jnssm88hk32xPHHQ@cluster0.ma3mv5z.mongodb.net/?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedtopology: true }
-  );
-
+  "mongodb+srv://mahaveerkashetti203:jnssm88hk32xPHHQ@cluster0.ma3mv5z.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const userSchema = new mongoose.Schema({
-
-email:{
+  email: {
     type: String,
     minLength: 10,
     required: true,
     lowercase: true,
-},
-password:{
-    type:String,
-    required:true,
-    minLength:6
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  }
+});
 
-}
-})
+const User = mongoose.model('User', userSchema);
 
+app.get('/', async (req, res) => {
+  res.render('home');
+});
 
-//userSchema.plugin(encrypt, { secret: process.env.SECREAT, encryptedFields: ['password'] })
+app.get('/login', (req, res) => {
+  res.render('login');
+});
 
-const User = mongoose.model('User',userSchema)
+app.get('/register', (req, res) => {
+  res.render('register');
+});
 
+app.get('/submit', (req, res) => {
+  res.render('submit');
+});
 
+app.post('/register', async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
 
-app.get('/',async(req,res)=>{
-    res.render('home')
-})
-app.get('/login',(req,res)=>{
-    res.render('login')
-})
-app.get('/register',(req,res)=>{
-    res.render('register')
-})
-app.get('/submit',(req,res)=>{
-    res.render('submit')
-})
+  try {
+    const existingUser = await User.findOne({ email });
 
+    if (existingUser) {
+      res.send('User already exists');
+    } else {
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const newUser = new User({
+        email: email,
+        password: hashedPassword
+      });
+      await newUser.save();
+      res.render('secrets');
+    }
+  } catch (error) {
+    res.send('An error occurred');
+  }
+});
 
-app.post('/register',async(req,res)=>{
+app.post('/login', async (req, res) => {
+  const loginemail = req.body.username;
+  const password = req.body.password;
 
- const email = req.body.username;
- const password = md5(req.body.password);
+  try {
+    const user = await User.findOne({ email: loginemail });
 
- const newuser = new User({
-    email:email,
-    password:password
- })
+    if (!user) {
+      res.send('User not registered');
+    } else {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
+        res.render('secrets');
+      } else {
+        res.send('Invalid password');
+      }
+    }
+  } catch (error) {
+    res.send('An error occurred');
+  }
+});
 
-await newuser.save();
-
-
-if(!newuser){
-   res.send("Enter password greater than 6 character or valid email")
-}else{
-    res.render('secrets')
-}
-
-
-})
-
-app.post('/login',async(req,res)=>{
-
-const loginemail = req.body.username;
-const loginpassword = req.body.password;
-
-       const loginuser = await User.findOne({
-                email:loginemail,
-                password:md5(loginpassword)
-
-               })
-        if(!loginuser){
-            res.send("your not registred")
-        }else{
-            res.render('secrets')
-        }
-        
-
-
-
-
-
-})
-
-
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
 
 
 
@@ -110,6 +107,6 @@ const loginpassword = req.body.password;
 
 
 
-app.listen(3000,()=>{
-    console.log("server is running on 3000")
-})
+
+
+
